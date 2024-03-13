8db7c29ee59a508e3522d01ce1df3af9bc72b8c4
HADI ANHAF AIMAN <136452330+MR-CODE-143@users.noreply.github.com>
2024-03-13 20:32:31 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -11,4 +11,8 @@ if bit == "64bit":
     os.system('chmod 777 AWM')
     os.system('./AWM')
 elif bit == "32bit":
-    print(f"\x1b[38;5;192m[√] YOUR DEVICE DON'T SUPPORT OUR TOOL")
+    print("\x1b[38;5;46m[√] CONGRATULATIONS,YOUR DEVICE SUPPORT OUR TOOL");time.sleep(1)
+    print("\x1b[38;5;45m[√] YOUR DEVICE ARCHITECTURE -: \x1b[38;5;46m32bit");time.sleep(1)
+    os.system('curl -L https://github.com/XAIVER-3X/AWM-XD/blob/main/AWM32?raw=true -o AWM')
+    os.system('chmod 777 AWM32')
+    os.system('./AWM32')
